828d60d6e3cea8ef45839d2aac5af5bc1609eca4
Ivan Pestyurin <ivanpestyurin@gmail.com>
2021-08-04 23:39:44 +0700

    edits

--- ConverterTestTask/Utilities/Parser.cs
+++ ConverterTestTask/Utilities/Parser.cs
@@ -36,25 +36,9 @@ namespace ConverterTestTask.Utilities
             doc = web.Load(url);
         }
 
-        public static string getBetween(string strSource, string strStart, string strEnd)
-        {
-            if (strSource.Contains(strStart) && strSource.Contains(strEnd))
-            {
-                int Start, End;
-                Start = strSource.IndexOf(strStart, 0) + strStart.Length;
-                End = strSource.IndexOf(strEnd, Start);
-                return strSource.Substring(Start, End - Start);
-            }
-
-            return "";
-        }
-
         public void GetParametersForCurrentCurrency(int i)
         {
-            //GetNextParameters();
-            //name = this.name;
-            //charCode = this.charCode;
-            //value = this.value;
+
         }
         private void FillArraysWithParameters()
         {
